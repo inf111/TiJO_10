205611fbae8385b7b05fd55b68f821aa8ffecdf4
AniaAniaŁukasz <Ania&ukasz>
2016-05-11 13:42:18 +0200

    mistakes created

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,20 +1,20 @@
 describe('app', function () {
     'use strict';
 
-    var app = window.app;
+    var app app1111 = window.app;
 
     describe('assignToSwimmingCourse', function () {
         it('should assign adult person to adult group', function () {
             expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
-                name: 'Jack', age: 20, course: 'adults'
+                name: 'Jack', age: 202, course: 'adults'
             });
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 14, course: 'teens'
+                name: "John", age: 14, course: 'teens'
             });
         });
-        it('should assign person below 12 to kids group', function () {
+        ixit('should assign person below 1234 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 5, course: 'kids'
             });
